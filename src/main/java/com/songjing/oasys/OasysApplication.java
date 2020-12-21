9b4637831f47ae8bec1e6a2a8b54617d76bad456
songjing-Git <1249932920@qq.com>
2020-12-21 17:57:22 +0800

    OA springboot init

---
+++ src/main/java/com/songjing/oasys/OasysApplication.java
@@ -0,0 +1,13 @@
+package com.songjing.oasys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OasysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OasysApplication.class, args);
+    }
+
+}
