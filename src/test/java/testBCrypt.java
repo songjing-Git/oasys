91b15f450c4b82e48f1aee95ee88aee4fbd985b3
songjing-Git <1249932920@qq.com>
2021-06-21 10:16:24 +0800

    项目第一阶段完成

---
+++ src/test/java/testBCrypt.java
@@ -0,0 +1,24 @@
+import com.songjing.oasys.OasysApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.Resource;
+
+/**
+ * @author songjing
+ * @version 1.0
+ * @date 2021/5/9 15:16
+ */
+
+@SpringBootTest(classes = OasysApplication.class)
+public class testBCrypt {
+
+    @Resource
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    void testBCrypt(){
+        System.out.println(passwordEncoder.encode("jw"));
+    }
+}
